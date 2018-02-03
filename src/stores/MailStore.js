8da95866a696ad8470fa58df9735c09fbb053c17
Zachary Hill <zach@zacharyhill.xyz>
2018-02-02 20:57:48 -0600

    Setup dispatcher for flux

--- src/stores/MailStore.js
+++ src/stores/MailStore.js
@@ -1,6 +1,7 @@
 import { EventEmitter } from 'events';
-
 import axios from 'axios';
+
+import dispatcher from '../dispatcher';
 
 class MailStore extends EventEmitter {
 
@@ -27,8 +28,21 @@ class MailStore extends EventEmitter {
     // use axios method when we get to async redux in videos
   }
 
+  handleActions(action) {
+    console.log('MailStore received an action', action); // for testing
+    switch(action.type) {
+      case 'CREATE_MAIL': {  // also just for testing
+        this.createMail(action.text);
+      }
+    }
+  }
+
 }
 
 const mailStore = new MailStore();
+dispatcher.register(mailStore.handleActions.bind(mailStore));
+
+//for testing
+window.dispatcher = dispatcher;
 
 export default mailStore;
